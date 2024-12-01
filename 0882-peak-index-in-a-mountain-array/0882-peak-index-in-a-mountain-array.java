50f642bf932330f6abf4b279e85bd79290bbc0bb
Garimab-12 <128513628+Garimab-12@users.noreply.github.com>
2024-12-02 00:47:29 +0530

    Time: 0 ms (100%), Space: 56.6 MB (15.92%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,13 +1,21 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int max=0;
+        int left = 0;
+        int right = arr.length - 1;
 
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>arr[max]){
-                max=max+1;
+        while (left < right) {
+            int mid = left + (right - left) / 2; // Prevents integer overflow
+            
+            if (arr[mid] > arr[mid + 1]) {
+                // If current element is greater than the next, the peak is on the left side or at mid
+                right = mid;
+            } else {
+                // If current element is smaller than the next, the peak is on the right side
+                left = mid + 1;
             }
-
         }
-        return max;
+
+        // left and right converge to the peak index
+        return left;
     }
 }
