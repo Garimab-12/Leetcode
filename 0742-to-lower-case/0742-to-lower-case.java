7a31117b734e2649adf1b4fdca28a1adc8d129f0
Garimab-12 <128513628+Garimab-12@users.noreply.github.com>
2024-12-01 20:29:23 +0530

    Time: 0 ms (100%), Space: 41.7 MB (34.8%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,7 +1,30 @@
 class Solution {
     public String toLowerCase(String s) {
-        
-        s=s.toLowerCase();
-        return s;
-    }
-}
+
+        StringBuilder st=new StringBuilder();
+        int l=s.length();
+        for(int i=0;i<l;i++){
+            char ch=s.charAt(i);
+            if(ch>='A'&& ch<='Z'){
+               
+               st.append((char)(ch+32));
+            }
+            else{
+                st.append(ch);
+
+            }
+
+
+
+        }
+        return st.toString();
+    }}
+
+
+
+
+
+
+
+
+                          
