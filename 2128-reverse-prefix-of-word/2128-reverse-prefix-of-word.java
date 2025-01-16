2bb90521a857f79222bdb524d175f667636ba017
Garimab-12 <128513628+Garimab-12@users.noreply.github.com>
2025-01-16 19:46:20 +0530

    Time: 1 ms (51.95%), Space: 41.7 MB (59.38%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
@@ -1,11 +1,9 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        
-      
-
 
         int l=word.indexOf(ch);
         if(l!=-1){ 
+            
         StringBuilder st=new StringBuilder();
          for(int j=l;j>=0;j--){
             
